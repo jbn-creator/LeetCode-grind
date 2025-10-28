05bb8b63ce1baaa29352231111c9dec948f000b7
joshua Bakata <joshuabakata@gmail.com>
2025-10-28 22:49:50 +0000

    Time: 129 ms (27.24%), Space: 26.9 MB (77.76%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        buy,profit = prices[0],0
+        # buy,profit = prices[0],0
 
         # if len(prices) == 2
         #   prices[1] > prices[0]:
@@ -8,18 +8,27 @@ class Solution:
         #     return profit
         # else:
         #     return 0
-        for i in range(len(prices)):
-            buy = min(prices[i], buy)
-            profit = max(profit, prices[i] - buy)
-        return profit
-        # for i in range(1, len(prices)):
-        #     sell = i
-        #     while prices[sell] - prices[buy] < profit and sell > buy:
-        #         buy += 1 
-        #         if prices[sell] - prices[buy] < profit and sell > buy:
-        #             continue
-        #         else:
-        #              break
-        #     profit = prices[sell] - prices[buy]
+        # for i in range(len(prices)):
+        #     buy = min(prices[i], buy)
+        #     profit = max(profit, prices[i] - buy)
         # return profit
+        buy,sell,profit= 0,1,0
+        while sell < len(prices):
+            if prices[sell] < prices[buy] and (sell - buy) == 1:
+                buy += 1
+                sell += 1
+            elif prices[sell] < prices[buy]:
+                buy += 1
+            else:
+                profit = max(profit,prices[sell] - prices[buy])
+                sell += 1
+
+            # while prices[sell] - prices[buy] < profit and sell > buy:
+            #     buy += 1 
+            #     if prices[sell] - prices[buy] < profit and sell > buy:
+            #         continue
+            #     else:
+            #          break
+            # profit = prices[sell] - prices[buy]
+        return profit
             
