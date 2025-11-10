12db65317829a5c0959ed8eb87c0645b26afd2b5
joshua Bakata <joshuabakata@gmail.com>
2025-11-10 21:33:32 +0000

    Time: 0 ms (100%), Space: 18.2 MB (20.45%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -3,15 +3,17 @@ class Solution:
         citations.sort()
         maxH = 0
 
+        #edge cases when we have a single element in the list
         if len(citations) == 1 and citations[0] != 0:
             return 1
         if len(citations) == 1 and citations[0] == 0:
             return 0
 
+        #go through the sorted list
         for i in range(len(citations)):
             freq = len(citations) - i
             if  freq >= citations[i]:
-                maxH = max(citations[i], maxH)
+                maxH = citations[i]
             else:
                 maxH = max(freq, maxH)
         return maxH
