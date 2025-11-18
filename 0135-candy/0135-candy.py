61ce09c5930434b7684571123d1fd199d71a70db
joshua Bakata <joshuabakata@gmail.com>
2025-11-18 10:10:57 +0000

    Time: 29 ms (10.97%), Space: 20.2 MB (8.1%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,41 +1,6 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        #Trying without hint: got 14/50 test case
 
-        # biggerRating = []
-        # initialVal = ratings[0] > ratings[1]
-        # biggerRating.append(initialVal)
-        # for i in range(1,len(ratings)):
-        #     if i == len(ratings) - 1:
-        #         value = ratings[i] > ratings[i - 1]
-        #         biggerRating.append(value)
-        #         break
-        #     if ratings[i] > ratings[i - 1] or ratings[i] > ratings[i + 1]:
-        #         biggerRating.append(True)
-        #     else:
-        #         biggerRating.append(False)
-        # print(f"Rating Array: {biggerRating}")
-
-        # output = 0
-        # prev = 0
-        # for i in range(len(ratings)):
-        #     if not biggerRating[i]:
-        #         output += 1
-        #         prev = 1
-        #         print(f"add: {1} for step{i} because not bigger")
-        #     else:
-        #         if i == 0:
-        #             output += 2
-        #             prev = 2
-        #             print(f"add: {prev} for step{i} because initially bigger")
-        #         else:
-        #             output += prev + 1
-        #             prev += 1
-
-        #             print(f"add: {prev} for step{i} because bigger")
-        # return output
-
-        #hint
         outpt = []
         for k in range(len(ratings)):
             outpt.append(1)
