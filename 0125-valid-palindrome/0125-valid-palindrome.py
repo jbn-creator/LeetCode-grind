bb9ae895188b705997a09ae9de78539f1a8116bc
joshua Bakata <joshuabakata@gmail.com>
2026-02-02 17:44:45 +0000

    Time: 5 ms (88.13%), Space: 20.9 MB (18.93%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,19 +1,35 @@
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         answ = True
+#         l = 0
+#         r = len(s) - 1
+
+#         if len(s) < 2:
+#             return True
+#         while l <= r:
+#             while not s[l].isalnum() and l < r:
+#                 l += 1
+#             while not s[r].isalnum() and l < r:
+#                 r -= 1
+#             if s[l].lower() != s[r].lower():
+#                 answ = False
+#                 break
+#             l += 1
+#             r -= 1
+#         return answ
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        answ = True
-        l = 0
-        r = len(s) - 1
+        s = s.lower()
+        s = re.sub('[^a-zA-Z0-9]', '', s)
+        
+        left = 0
+        right = len(s) - 1
 
-        if len(s) < 2:
-            return True
-        while l <= r:
-            while not s[l].isalnum() and l < r:
-                l += 1
-            while not s[r].isalnum() and l < r:
-                r -= 1
-            if s[l].lower() != s[r].lower():
-                answ = False
-                break
-            l += 1
-            r -= 1
-        return answ
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            else:
+                left += 1
+                right -= 1
+        
+        return True
