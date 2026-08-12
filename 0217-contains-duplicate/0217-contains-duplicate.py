f7ccefdd84665cfc5fff6be859a808b9fe817336
joshua Bakata <joshuabakata@gmail.com>
2026-08-12 06:59:52 +0200

    Time: 34 ms (23.93%), Space: 27.3 MB (13.73%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        seen = set()
+        for i in range(len(nums)):
+            if nums[i] in seen:
+                return True 
+            else:
+                seen.add(nums[i])
+        return False
+
+        
