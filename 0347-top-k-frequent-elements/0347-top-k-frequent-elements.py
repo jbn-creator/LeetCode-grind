cdd5113b41c3c24021cd95e2d90b2cddd5ebe8bd
joshua Bakata <joshuabakata@gmail.com>
2026-08-15 14:40:11 +0200

    Time: 209 ms (5.01%), Space: 19.8 MB (23.77%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+
+        rec = {}
+        freq = [[] for i in range(len(nums) + 1)]
+        for num in nums:
+            if num not in rec:
+                rec[num] = 1
+            else:
+                rec[num] += 1
+        for key, v in rec.items():
+            freq[v].append(key)
+        print(freq)
+        answ = []
+        for i in range(len(freq) - 1, 0, - 1):
+            if freq[i] != []:
+                print(freq[i])
+                print(answ)
+                for n in freq[i]:
+                    answ.append(n)
+                    k -= 1
+                    if k == 0:
+                        return answ
