d576c8ceee8b97bed89285c26f4e1ebfccb1ffc1
joshua Bakata <joshuabakata@gmail.com>
2026-08-18 07:21:16 +0200

    Time: 12 ms (15.24%), Space: 12.5 MB (24.34%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        def colValid(sudoku):
+            for rawColumns in zip(*sudoku):
+                column =  [x for x in rawColumns if x != "."]
+                if len(set(column)) != len(column):
+                    return False
+            return True
+        def subBoxValid(sudoku):
+            #didn't know what to write for this one 
+            for r in range(0, 9, 3):
+                for c in range(0, 9, 3):
+                    box = [
+                        sudoku[r + i][c + j]
+                        for i in range(3)
+                        for j in range(3)
+                        if sudoku[r + i][c + j] != "."
+                    ]
+                    if len(set(box)) != len(box):
+                        return False
+            return True
+        def rowValid(sudoku):
+            for rawRow in sudoku:
+                row =  [x for x in rawRow if x != "."]
+                if len(set(row)) != len(row):
+                    return False
+            return True
+
+        
+        if colValid(board) and subBoxValid(board) and rowValid(board):
+            return True
+        return False
+
+    
+
