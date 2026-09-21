921c13b55d207cb09417ce907482068e1e2e1607
joshua Bakata <joshuabakata@gmail.com>
2026-09-21 19:56:10 +0100

    Time: 0 ms (100%), Space: 19.3 MB (93.06%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -3,10 +3,21 @@ class Solution:
         l, r = 0, len(nums) - 1
         while l <= r:
             mid = (l + r) // 2
-            if nums[l] <= nums[mid] and nums[mid] <= nums[r]:
+            if nums[l] <= nums[mid] and nums[mid] <= nums[r]: #the array is sorted return lefmost element
                 return nums[l]
-            elif nums[l] <= nums[mid]:
+            elif nums[l] <= nums[mid]: #the left part is sorted but not the right part thus the right contains the inflection point
                 l = mid + 1
             else:
-                r = mid
+                r = mid #the left is not sorted and contains inflection point
+
+            """
+            why do we not set r = mid - 1:
+
+            in the second if statement the right part is not sorted as mid is bigger than right pointer, thus we never need to look for mid again
+
+            in the third (else) if statement the left part is not sorted, mid is smaller than left, since mid could be the smallest element we keep it in the boundary instead of discarding it by doing r = mid - 1
+
+            """
+
+
         
